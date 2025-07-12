8523c74906a24d6b457c31d5c809ff3d4e2a8f54
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-07-12 23:30:31 +0100

    refactor: allow customisation of options

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface IGenerateOptions {
+    topTypeOptions?: string[];
+    accessoriesTypeOptions?: string[];
+    facialHairTypeOptions?: string[];
+    facialHairColorOptions?: string[];
+    clotheTypeOptions?: string[];
+    eyeTypeOptions?: string[];
+    eyebrowTypeOptions?: string[];
+    mouthTypeOptions?: string[];
+    skinColorOptions?: string[];
+    hairColorTypes?: string[];
+    hatColorOptions?: string[];
+    clotheColorOptions?: string[];
+}
